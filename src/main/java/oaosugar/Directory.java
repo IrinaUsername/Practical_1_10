de74e7f63f5e25c000e247a0e2574d94f79d5d46
IrinaUsername <milovantseva.i.a@edu.mirea.ru>
2024-03-27 23:49:41 +0300

    Meow

---
+++ src/main/java/oaosugar/Directory.java
@@ -0,0 +1,17 @@
+package oaosugar;
+
+public class Directory {
+    private String directoryOfFile;
+
+    public Directory (String directoryOfFile){
+        this.directoryOfFile = directoryOfFile;
+    }
+
+    public String getDirectoryOfFile() {
+        return directoryOfFile;
+    }
+
+    public void setDirectoryOfFile(String directoryOfFile) {
+        this.directoryOfFile = directoryOfFile;
+    }
+}
